7a1c6830082f0721019350e2d6153a51c59fcda2
Shauna Myers <shaunamyers26@gmail.com>
2022-04-19 17:58:39 -0700

    Complete any_patern_spec exercise

--- enumerables/exercises_1/spec/any_pattern_spec.rb
+++ enumerables/exercises_1/spec/any_pattern_spec.rb
@@ -2,51 +2,57 @@ RSpec.describe 'Any Pattern Test' do
 
   it 'has at least one zero' do
     numbers = [2, 0, 9, 3, 0, 1]
-    has_zero = false
-    numbers.each do |number|
-      has_zero = true if number.zero?
+    # has_zero = false
+    # numbers.each do |number|
+    #   has_zero = true if number.zero?
+    # end
+
+    has_zero = numbers.any? do |num|
+      num == 0
     end
+
     expect(has_zero).to eq(true)
   end
 
-  xit 'does not have any zeros' do
+  it 'does not have any zeros' do
     numbers = [3, 1, 3, 2, 4, 9, 8]
-    has_zero = false
-    numbers.each do |number|
-      # Your code goes here
-    end
+    # an even easier way to write the above code:
+    has_zero = numbers.any?(0)
+
     expect(has_zero).to eq(false)
   end
 
-  xit 'has at least one alice' do
+  it 'has at least one alice' do
     names = ["Bill", "Bob", "Burton", "Alice", "Brandon"]
-    has_alice = false
-    # Your code goes here
+    has_alice = names.any?("Alice")
     expect(has_alice).to eq(true)
   end
 
-  xit 'no alices' do
+  it 'no alices' do
     names = ["Chuck", "Charlene", "Cory", "Chris", "Carl"]
-    # Your code goes here
+    has_alice = names.any?("Alice")
     expect(has_alice).to eq(false)
   end
 
-  xit 'has a multi word phrase' do
+  it 'has a multi word phrase' do
     phrases = ["Sure!", "OK.", "I have no idea.", "Really?Whatever."]
-    # Your code goes here
+    has_multi_word_phrase = phrases.any? do |phrase|
+      phrase.include?(' ')
+    end
     expect(has_multi_word_phrase).to eq(true)
   end
 
-  xit 'has no monkeys' do
+  it 'has no monkeys' do
     animals = ["elephant", "hippo", "jaguar", "python"]
-    # Your code goes here
+    has_monkeys = animals.any?("monkey")
     expect(has_monkeys).to eq(false)
   end
 
-  xit 'has no multiples of five' do
+  it 'has no multiples of five' do
     numbers = [3, 1, 3, 2, 4, 9, 8]
-    # Your code goes here
+    multiples_of_5 = numbers.any? do |num|
+      num % 5 == 0
+    end
     expect(multiples_of_5).to eq(false)
   end
 end
-
